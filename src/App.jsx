99d62b2551180089837baef4c1caf29941d45c4b
Rupeshkashyap2 <rupeshkashyaprupeshkashyap412@gmail.com>
2026-08-31 01:24:52 +0530

    Adding of my-booking

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,32 @@
-import React  from "react";
+import React from "react";
 import Navbar from "./components/Navbar";
 import { Route, Routes, useLocation } from "react-router-dom";
 import Home from "./pages/Home";
 import Footer from "./components/Footer";
+import AllRooms from "./pages/AllRooms";
+import RoomDetails from "./pages/RoomDetails";
+import MyBookings from "./pages/MyBookings";
 
-const App  = () => {
+const App = () => {
 
-  const isOwnerPath = useLocation().pathname.includes("owner");
+    const isOwnerPath = useLocation().pathname.includes("owner");
 
-  return (
-    <div>
-      {!isOwnerPath && <Navbar />}
-      <div className='min-h-[70vh]'>
-        <Routes>
-            <Route path='/' element = {<Home/>}/>
-        </Routes>
+    return (
+        <div>
+            {!isOwnerPath && <Navbar />}
 
-      </div>
-      <Footer/>
-    </div>
-  )
+            <div className='min-h-[70vh]'>
+                <Routes>
+                    <Route path='/' element={<Home />} />
+                    <Route path='/rooms' element={<AllRooms />} />
+                    <Route path='/rooms/:id' element={<RoomDetails />} />
+                    <Route path='/my-bookings' element={<MyBookings />} />
+                </Routes>
+            </div>
+
+            <Footer />
+        </div>
+    )
 }
 
 export default App

21fae499184f03de6d917adf5c1375ff7056ead1
Rupeshkashyap2 <rupeshkashyaprupeshkashyap412@gmail.com>
2026-09-04 12:31:49 +0530

    AddRoom,Dashbord

--- src/pages/hotelOwner/AddRoom.jsx
+++ src/pages/hotelOwner/AddRoom.jsx
@@ -1,10 +1,99 @@
-import React from "react";
+import React, { useState } from "react";
+import Title from "../../components/Title";
+import { assets } from "../../assets/assets";
 
 const AddRoom = () => {
-    return (
-        <div>
 
-        </div>
+    const [image, setImage] = useState({
+        1: null,
+        2: null,
+        3: null,
+        4: null,
+    })
+
+    const [input, setInput] = useState({
+        roomType: "",
+        pricePerNight: 0,
+        amenities: {
+            'Free Wi-Fi': false,
+            'Air Conditioning': false,
+            'Room Service': false,
+            'Free Breakfast': false,
+            'Mountain View': false,
+            'pool Access': false,
+            
+        }
+    })
+
+    return (
+        <form>
+            <Title align='left' font='outfit' title='Add Room' subTitle='Fill in the 
+            details carefully and accurate room details, pricing , and amenities, to 
+            enhance the user booking experience'/>
+
+            {/* Upload Image Section */}
+            <p className="text-gray-800 mt-10">Images</p>
+            <div className="grid grid-cols-2 sm:flex gap-4 my-2 flex-wrap">
+                {Object.keys(image).map((key) => (
+                    <label htmlFor={`roomImage${key}`} key={key}>
+                        <img className='max-h-13 cursor-pointer opacity-80'
+                        src={image[key] ? URL.createObjectURL(image[key]) : assets.uploadArea} alt=""/>
+                        <input type="file" accept="image/*" id={`roomImage${key}`} hidden
+                        onChange={e=> setImage({...image, [key]: e.target.files[0]})}/>
+                    </label>
+                ))}
+            </div>
+
+            <div className="w-full flex max-sm:flex-col sm:gap-4 mt-4">
+                <div className="flex-1 max-w-48">
+                    <p className="text-gray-800 mt-4">Room Type</p>
+                    <select value={input.roomType} onChange={e=> setInput({...input, roomType: e.target.value})}
+                    className="border border-gray-300 mt-1 rounded p-2 w-full text-gray-500">
+                        <option value="">Select Room Type</option>
+                        <option value="Single">Single</option>
+                        <option value="Double">Double</option>
+                        <option value="Suite">Suite</option>
+                        <option value="Deluxe">Deluxe</option>
+                    </select>
+                </div>
+
+                <div className="flex-1 max-w-48">
+                    <p className="text-gray-800 mt-4">
+                        Price <span className="text-xs">/ Night</span>
+                    </p>
+                    <input type="number" value={input.pricePerNight} onChange={e=> setInput({...input, pricePerNight: e.target.value})} 
+                    className="border border-gray-300 mt-1 rounded p-2 w-full text-gray-500" placeholder="Enter Price"/>
+                </div>
+
+            </div>
+
+            <p className="text-gray-800 mt-4">Amenities</p>
+            <div className="flex flex-col flex-wrap mt-1 text-gray-500 max-w-sm">
+                {Object.keys(input.amenities).map((amenity, index) => (
+                    <div key={index} className="flex items-center gap-2">
+                        <input
+                            type="checkbox"
+                            checked={input.amenities[amenity]}
+                            onChange={(e) =>
+                                setInput({
+                                    ...input,
+                                    amenities: {
+                                        ...input.amenities,
+                                        [amenity]: e.target.checked
+                                    }
+                                })
+                            }
+                        />
+                        <label className="text-gray-500">{amenity}</label>
+                    </div>
+                ))}
+
+            </div>
+
+            <button className="bg-blue-500 text-white px-4 py-2 rounded mt-6 hover:bg-blue-600 transition-colors">
+                Add Room
+            </button>
+        </form>
     )
 }
 
--- src/pages/hotelOwner/Dashboard.jsx
+++ src/pages/hotelOwner/Dashboard.jsx
@@ -8,26 +8,40 @@ const Dashboard = () => {
 
     return (
         <div>
-            <Title align='left' font='outline' title="Dashboard" subTitle="Monitor your
-            room listings, track bookings and analyze revenue-all in one place. Stay
-            updated with real-time insights to ensure smooth operations." />
+            <Title
+                align='left'
+                title="Dashboard"
+                subTitle="Monitor your room listings, track bookings and analyze revenue-all in one place. Stay updated with real-time insights to ensure smooth operations."
+            />
 
             <div className="flex gap-8 my-8">
                 {/* -------Total Bookings Card ------- */}
                 <div className="bg-primary/3 border border-primary/10 rounded flex p-4 pr-8">
-                    <img src={assets.totalBookingIcon} alt="" className="max-sm:hidden h-10" />
+                    <img
+                        src={assets.totalBookingIcon}
+                        alt=""
+                        className="max-sm:hidden h-10"
+                    />
                     <div className="flex flex-col sm:ml-4 font-medium">
                         <p className="text-blue-500 text-lg">Total Bookings</p>
-                        <p className="text-neutral-400 text-base">{dashboardData.totalBookings}</p>
+                        <p className="text-neutral-400 text-base">
+                            {dashboardData.totalBookings}
+                        </p>
                     </div>
                 </div>
 
                 {/* -------Total Revenue Card ------- */}
                 <div className="bg-primary/3 border border-primary/10 rounded flex p-4 pr-8">
-                    <img src={assets.totalRevenueIcon} alt="Total Revenue" className="max-sm:hidden h-10" />
+                    <img
+                        src={assets.totalRevenueIcon}
+                        alt="Total Revenue"
+                        className="max-sm:hidden h-10"
+                    />
                     <div className="flex flex-col sm:ml-4 font-medium">
                         <p className="text-blue-500 text-lg">Total Revenue</p>
-                        <p className="text-neutral-400 text-base">${dashboardData.totalRevenue}</p>
+                        <p className="text-neutral-400 text-base">
+                            ${dashboardData.totalRevenue}
+                        </p>
                     </div>
                 </div>
             </div>
@@ -41,34 +55,57 @@ const Dashboard = () => {
                 <table className="w-full">
                     <thead className="bg-gray-50">
                         <tr className="text-gray-800 font-medium">
-                            <th className="py-3 px-4 text-left-800 font-medium ">User Name</th>
-                            <th className="py-3 px-4 text-left-800 font-medium max-sm:hidden">Room Name</th>
-                            <th className="py-3 px-4 text-left-800 font-medium text-center">Total Amount</th>
-                            <th className="py-3 px-4 text-left-800 font-medium text-center">Payment Status</th>
+                            <th className="py-3 px-4 text-left">
+                                User Name
+                            </th>
+                            <th className="py-3 px-4 text-left max-sm:hidden">
+                                Room Name
+                            </th>
+                            <th className="py-3 px-4 text-center">
+                                Total Amount
+                            </th>
+                            <th className="py-3 px-4 text-center">
+                                Payment Status
+                            </th>
                         </tr>
                     </thead>
 
+                    <tbody className="text-sm">
+                        {dashboardData.bookings.map((item, index) => (
+                            <tr
+                                key={index}
+                                className="border-b border-gray-200"
+                            >
+                                <td className="py-3 px-4 text-gray-700 border-t border-gray-300">
+                                    {item.user.username}
+                                </td>
 
-                    <tbody className='text-sm'>
-                          {dashboardData.bookings.map((item,index) => (
-                            <tr key={index} className="border-b border-gray-200">
-                              <td className="py-3 px-4">{item.userName}</td>
-                              <td className="py-3 px-4 max-sm:hidden">{item.roomName}</td>
-                              <td className="py-3 px-4 text-center">${item.totalAmount.toFixed(2)}</td>
-                              <td className="py-3 px-4 text-center">
-                                <span className={`px-2 py-1 rounded-full text-xs font-medium ${
-                                  item.paymentStatus === "Paid" ? "bg-green-100 text-green-800" : "bg-red-100 text-red-800"
-                                }`}>
-                                  {item.paymentStatus}
-                                </span>
-                              </td>
+                                <td className="py-3 px-4 text-gray-700 border-t border-gray-300 max-sm:hidden">
+                                    {item.room.roomType}
+                                </td>
+
+                                <td className="py-3 px-4 text-gray-700 border-t border-gray-300 text-center">
+                                    ${item.totalPrice}
+                                </td>
+
+                                <td className="py-3 px-4 text-center border-t border-gray-300">
+                                    <button
+                                        className={`py-1 px-3 text-xs rounded-full ${
+                                            item.isPaid
+                                                ? "bg-green-200 text-green-600"
+                                                : "bg-amber-200 text-yellow-600"
+                                        }`}
+                                    >
+                                        {item.isPaid ? "Completed" : "Pending"}
+                                    </button>
+                                </td>
                             </tr>
-                          ))}
+                        ))}
                     </tbody>
                 </table>
             </div>
         </div>
-    )
-}
+    );
+};
 
 export default Dashboard;
